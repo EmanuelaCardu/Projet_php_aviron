2312883d8165349b1ef48b0a4f2a8730db4e0fb9
Emanuela Cardu <emanuelacardu@gmail.com>
2023-09-20 11:59:58 +0200

    dopo il punto 4

---
+++ bdMigrateFill.bat
@@ -0,0 +1,6 @@
+
+
+symfony console make:migration
+symfony console doctrine:migrations:migrate
+
+@REM symfony console doctrine:fixtures:load --append
---
+++ bdfixtures.bat
@@ -0,0 +1,2 @@
+@REM Lancer le fixture efface le tableau
+symfony console doctrine:fixtures:load --no-interaction
