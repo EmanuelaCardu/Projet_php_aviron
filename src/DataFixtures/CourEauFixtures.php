d09722e3cee675a018bb69ac8562f4f5d8e8df9f
Emanuela Cardu <emanuelacardu@gmail.com>
2023-09-26 09:26:31 +0200

    apres bd CourEau

--- src/DataFixtures/CourEauFixtures.php
+++ src/DataFixtures/CourEauFixtures.php
@@ -2,13 +2,25 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\CourEau;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class CourEauFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create();
+        for ($i = 0; $i < 10 ; $i++){
+            $courEau = new CourEau();
+            $courEau->setNom ("courEau".$i);
+            
+            
+            $courEau->setDescription($faker->text());
+            $courEau->setDistance(20 +$i);
+            $manager->persist ($courEau);
+        }
         // $product = new Product();
         // $manager->persist($product);
 
