c2ec0401811772f48aba3f7637b4bd8697fc4b3c
Emanuela Cardu <emanuelacardu@gmail.com>
2023-10-05 14:14:21 +0200

    adition km dom

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,9 +15,9 @@ class HomeController extends AbstractController
     public function AfficheKmUser(ManagerRegistry $doctrine)
     {
         $em = $doctrine->getManager();
-        $query =$em->createQuery("SELECT km FROM App\Entity\User km");
+        $query = $em->createQuery("SELECT km FROM App\Entity\User km");
         $resultat = $query->getResult();
-        $vars = ['km'=> $resultat];
+        $vars = ['km' => $resultat];
 
         return $this->render('accueil/accueil_user.html.twig', $vars);
     }
@@ -34,4 +35,21 @@ class HomeController extends AbstractController
 
         return $this->render('home/home.html.twig', $vars);
     }
+
+    #[Route("/recup/km/form", name: "recup_km_form")]
+    public function recupKmForm(Request $req)
+    {
+        $km = $req->request->get('km'); 
+
+        return $this->render(
+            "accueil/accueil_user.html.twig",['km' => $km]);
+    }
+
+
+    #[Route("/form/affiche/km")]
+public function FormAfficheKm()
+{
+    //faire ici addition des km?
+    return $this->render("/accueil_user.html.twig");
+}
 }
