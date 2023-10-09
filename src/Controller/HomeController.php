e8c63aaf9c78eec77d82667874ea43fdc4be1c4e
Emanuela Cardu <emanuelacardu@gmail.com>
2023-10-09 14:16:26 +0200

    cours validees non validees

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -34,7 +34,7 @@ class HomeController extends AbstractController
         $resultat = $query->getResult();
         $vars = ['cours' => $resultat];
         //creer ici le bouton "voir"
-        
+
 
         return $this->render('home/home.html.twig', $vars);
     }
@@ -42,18 +42,17 @@ class HomeController extends AbstractController
     #[Route("/form/km/saisis", name: "form_km_saisis")]
     public function recupKmForm(Request $req, ManagerRegistry $doctrine)
     {
-        
-        
+
+
         $km = $req->request->get('km');
 
         $user = $this->getUser();
-        $user->setKm ($user->getKm() + $km);
+        $user->setKm($user->getKm() + $km);
         $em = $doctrine->getManager();
         $em->persist($user);
         $em->flush();
 
-        return new JsonResponse(['km'=> $user->getKm()]);
-  
+        return new JsonResponse(['km' => $user->getKm()]);
     }
 
 
@@ -64,9 +63,16 @@ class HomeController extends AbstractController
         return $this->render("/accueil_user.html.twig");
     }
 
-    #[Route ("affiche/photo/{id}")]
-    public function affichePhoto(Request $res){
+    #[Route("affiche/photo/{id}")]
+    public function affichePhoto(Request $res)
+    {
         //prendre ici les photos qui sont liés à l'id des CourEaux
+
+        // prendre le cours d'eau de la bd find($id)
+        // $cours->getPhoto() // ATTENTION CEST UNE COLLECTION
+        // renvoyer les photos a la vue dans un JsonResponse
+
+
     }
 
     // ici je prend tous les cours pour les afficher dans le accordion2
@@ -74,14 +80,34 @@ class HomeController extends AbstractController
     public function accordionCours(ManagerRegistry $doctrine)
     {
         $em = $doctrine->getManager();
-        $query = $em->createQuery("SELECT cour FROM App\Entity\CourEau cour  ");
+        $query = $em->createQuery("SELECT cour FROM App\Entity\CourEau cour");
         $resultat = $query->getResult();
-        $vars = ['AccCours' => $resultat];
-        
-        
+
+
+        // separer en valide et non validees
+        $coursValidees = [];
+        $coursNonValidees = [];
+        $user = $this->getUser();
+
+        foreach ($resultat as $cours) {
+            // voir si le user connecte est dans la liste de users de cette cours
+            if ($cours->getUser()->contains($user)) {
+                $coursValidees[] = $cours;
+            } else {
+                // l'user n'est pqs dans la liste
+                $coursNonValidees[] = $cours;
+            }
+        }
+        dump($coursNonValidees);
+        dd($coursValidees);
+
+        $vars = [
+            'coursValidees' => $coursValidees,
+            'coursNonValidees' => $coursNonValidees
+        ];
+
+
 
         return $this->render('accueil/accueil_user_accordion2.html.twig', $vars);
     }
-
-
 }
